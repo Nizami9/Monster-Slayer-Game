ed6a4220b925366844e7753fbc8628ea4f2660ef
Nizami Suleymanov <nizami.suleymanoff@gmail.com>
2023-02-15 16:25:47 +0100

    Adding a Game Over Screen

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const app = Vue.createApp({
         return {
             playerHealth: 100,
             monsterHealth: 100,
-            currentRound: 0
+            currentRound: 0,
+            winner: null
         }
     },
     computed: {
@@ -20,6 +21,24 @@ const app = Vue.createApp({
         mayUseSpecailAttack() {
             return this.currentRound % 3 !== 0;
         }
+    },
+
+    watch: {
+        playerHealth(value) {
+            if (value <=0 && this.monsterHealth <=0) {
+                return this.winner = 'draw'
+            } else if (value <=0) {
+                return this.winner = 'monster'
+            }
+        },
+        
+        monsterHealth(value) {
+            if (value <=0 && this.playerHealth <=0) {
+                return this.winner = 'draw'
+            } else if (value <=0) {
+                return this.winner = 'player'
+            }
+            }
     },
     methods: {
         attackMonster() {
